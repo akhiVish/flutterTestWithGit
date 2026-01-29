fed0a61d8a68123c5298a9107dbc1b04a4c7244c
akhiVish <akhivk24pm@gmail.com>
2026-01-29 20:14:04 +0530

    Add new text in B

--- lib/screen_b.dart
+++ lib/screen_b.dart
@@ -12,7 +12,27 @@ class __ScreenBPagStateState extends State<_ScreenBPagState> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Screen B')),
-      body: const Center(child: Text('This is Screen B')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('This is Screen B'),
+
+          const SizedBox(height: 20),
+
+          const Text('More content can go here.'),
+
+          const SizedBox(height: 20),
+          const Text('Even more content here.'),
+
+          ElevatedButton(onPressed: null, child: const Text('A Button')),
+
+          const SizedBox(height: 20),
+          const Text('End of Screen B content.'),
+
+          Text('Additional line 1.'),
+          Text('Additional line 2.'),
+        ],
+      ),
     );
   }
 }
